1603f14e0c01d46f07ce9dba0f06b7d0d17bff38
yixun-h <61463722+yixun-h@users.noreply.github.com>
2020-03-26 19:45:56 +0100

    Add files via upload

---
+++ seqlogo_ggplot.R
@@ -0,0 +1,39 @@
+rm(list = ls())
+#set workdesk
+setwd("~/Desktop/bioinformatics/smallpro")
+#load the paackage
+require(ggplot2)
+require(reshape2)
+# remove the first line (idline) in BASH by the following code: 
+# 'tail -n +2 MA0007.1.pfm >> MA0007.pfm'
+# loads pfm matrix
+data<-scan("MA0007.pfm")
+mat <- matrix(data,byrow=TRUE,nrow=4,dimnames=list(c('A','C','G','T')))
+#transfer matrix to datafarme
+freqdf <- as.data.frame(t(mat))
+#convert the frequency data into percentage data
+freqdf$total<-rowSums(freqdf)
+options(digits = 1)
+freqdf$heiA<-freqdf$A/freqdf$total
+freqdf$heiC<-freqdf$C/freqdf$total
+freqdf$heiG<-freqdf$G/freqdf$total
+freqdf$heiT<-freqdf$T/freqdf$total
+#add the position column into dataframe
+freqdf$pos = as.numeric(as.character(rownames(freqdf)))
+#calculate the height for each letter
+logodf <- data.frame(A=freqdf$heiA, C=freqdf$heiC,
+                     G=freqdf$heiG, T=freqdf$heiT, 
+                     pos=freqdf$pos)
+#Convert an object into a molten data frame.
+lmf <- melt(logodf, id.var='pos')
+#plot tp the screen(alternative)
+quartz(height=3, width=8)
+#plot the seqlogo
+ggplot(data=lmf, aes(x=as.numeric(as.character(pos)), y=value))  +
+  geom_bar(aes(fill=variable,order=value), position='stack', 
+           stat='identity', alpha=0.6) +
+  xlab('position')+
+  ylab('probability')+
+  theme_bw()
+#save output
+quartz.save('StackOverflow_5438474.png', type='png')
