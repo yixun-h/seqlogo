ffeec4265f9cf3e2a0f9e963e3b060e7b3987a65
yixun-h <61463722+yixun-h@users.noreply.github.com>
2020-03-27 17:57:11 +0100

    Update seqlogo_ggplot.R

--- seqlogo_ggplot.R
+++ seqlogo_ggplot.R
@@ -20,20 +20,35 @@ freqdf$heiG<-freqdf$G/freqdf$total
 freqdf$heiT<-freqdf$T/freqdf$total
 #add the position column into dataframe
 freqdf$pos = as.numeric(as.character(rownames(freqdf)))
-#calculate the height for each letter
-logodf <- data.frame(A=freqdf$heiA, C=freqdf$heiC,
+#calculate the height for each letter in probalibilty
+logopr <- data.frame(A=freqdf$heiA, C=freqdf$heiC,
                      G=freqdf$heiG, T=freqdf$heiT, 
                      pos=freqdf$pos)
+#add##calculate and define the height for each letter in bits
+freqdf$height <- apply(freqdf[,c('heiA', 'heiC','heiG','heiT')], MARGIN=1,
+                       FUN=function(x){2+sum(log(x^x, base = 2))})
+logobit <- data.frame(A=freqdf$heiA*freqdf$height, C=freqdf$heiC*freqdf$height,
+                     G=freqdf$heiG*freqdf$height, T=freqdf$heiT*freqdf$height, 
+                     pos=freqdf$pos)
 #Convert an object into a molten data frame.
-lmf <- melt(logodf, id.var='pos')
+mf_pr <- melt(logopr, id.var='pos')
+mf_bit <- melt(logobit, id.var='pos')
+
 #plot tp the screen(alternative)
 quartz(height=3, width=8)
-#plot the seqlogo
-ggplot(data=lmf, aes(x=as.numeric(as.character(pos)), y=value))  +
+#plot the seqlogo in probability
+ggplot(data=mf_pr, aes(x=as.numeric(as.character(pos)), y=value))  +
   geom_bar(aes(fill=variable,order=value), position='stack', 
            stat='identity', alpha=0.6) +
   xlab('position')+
   ylab('probability')+
+  theme_bw()
+#plot the seqlogo in bits
+ggplot(data=mf_bit, aes(x=as.numeric(as.character(pos)), y=value))  +
+  geom_bar(aes(fill=variable,order=value), position='stack', 
+           stat='identity', alpha=0.6) +
+  xlab('position')+
+  ylab('bit')+
   theme_bw()
 #save output
 quartz.save('StackOverflow_5438474.png', type='png')
